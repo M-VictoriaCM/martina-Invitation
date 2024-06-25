7d40f6dc7a8ea712adfdc252e16206f9d5ae58bc
Maria Victoria <mvittoria7@gmail.com>
2024-06-25 11:14:51 -0300

    modificacion

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,23 +3,23 @@ import { ref } from 'vue';
 import LoadingV from '../components/LoadingV.vue';
 import Portada from '../components/Portada.vue';
 import FilmDinamic from '../components/FilmDinamic.vue';
-import ReflectoresSuperiores from '../assets/img/reflectores_superiores.webp';
+import ReflectorSuperior from '../assets/img/reflectores_superiores.webp';
 
 export default {
     name: 'Home',
-    components: { LoadingV, FilmDinamic, Portada },
     data(){
         return{
-            ReflectoresSuperiores,
-            animate:false
+            ReflectorSuperior
         }
     },
+    components: { LoadingV, FilmDinamic, Portada },
     setup() {
         const showLoading = ref(true);
 
         function onCountdownFinished() {
             showLoading.value = false;
         }
+
         return {
             showLoading,
             onCountdownFinished
@@ -32,49 +32,45 @@ export default {
         <loading-v v-if="showLoading" @countdown-finished="onCountdownFinished"></loading-v>
         <div v-else class="main-content">
             <film-dinamic class="film1" />
-            <div class="home_container" :class="{'fade-in': animate}">
+            <div class="home_container">
                 <div class="img__reflect"></div>
-                <img class="reflect" v-lazy="ReflectoresSuperiores" alt="reflectores_superiores">
+                <img class="reflect" v-lazy="ReflectorSuperior" alt="reflectores_superiores">
             </div>
-            <div :class="{ 'fade-in': animate }">
-                <portada></portada>
-            </div>
+            <portada></portada>
         </div>
     </div>
 </template>
 
 
 <style scoped>
-#home,
-.main-content,
-.home_container {
+#home {
     display: flex;
+    justify-content: center;
     align-items: center;
     width: 100%;
-}
-
-.main-content,
-.reflect {
-    height: 100%;
-}
-
-#home {
-    justify-content: center;
     height: 100vh;
 }
 
 .main-content {
+    display: flex;
     flex-direction: column;
+    align-items: center;
     justify-content: space-between;
+    width: 100%;
+    height: 100%;
 }
 
 .home_container {
+    display: flex;
     flex-direction: row;
     justify-content: center;
+    width: 100%;
+    align-content: center;
 }
 
 .reflect {
     width: 450px;
+    height: 100%;
     position: relative;
     z-index: 2;
     top: 0;
@@ -83,27 +79,16 @@ export default {
 .img__reflect {
     display: none;
 }
-@keyframes fade-in {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-}
 
-.fade-in {
-  animation: fade-in 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
-}
-
+/*Mobile */
 @media(max-width:360px) {
-
-    #home,
-    .reflect {
+    #home {
         width: 100%;
     }
+    .reflect{
+        width:100%;
+    }
 }
-
 @media (min-width: 481px) and (max-width: 767px) {
     .reflect {
         display: none;
@@ -122,6 +107,7 @@ export default {
     }
 }
 
+
 @media (min-width: 768px) and (max-width: 1024px) {
     .reflect {
         display: none;
@@ -132,23 +118,10 @@ export default {
         background-image: url('../assets/img/reflectores_superiores.webp');
         background-repeat: repeat-x;
         background-size: contain;
-        width: 100%;
+        width: 100vw;
         height: 100vh;
         position: relative;
         z-index: 1;
-    }
-}
-@media (min-width: 1022px) {
-    .reflect {
-        display: none;
-    }
-
-    .img__reflect {
-        display: block;
-        background-image: url('../assets/img/reflectores_superiores2.webp');
-        background-repeat: repeat-x;
-        background-size: contain;
-        width: 100vw;
     }
 }
 
@@ -157,6 +130,7 @@ export default {
         height: 100vh;
     }
 
+
     .reflect {
         display: none;
     }
@@ -168,9 +142,8 @@ export default {
         background-size: contain;
         width: 100vw;
         height: 100vh;
-        /* position: relative; */
+        position: relative;
         z-index: 1;
-        overflow: hidden;
     }
 }
 </style>
